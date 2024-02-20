ec8911e435d386cff6381d55359f5f90a7cb7761
christellebaptiste <143545341+christellebaptiste@users.noreply.github.com>
2024-02-20 15:57:04 -0500

    Add files via upload

---
+++ pyton3/PE3_2.py
@@ -0,0 +1,11 @@
+#Calculates the amount of a server’s tip. Save the code as PE3_2.py.
+#a) Prompt and request input the amount of the bill (in float) and the percentage of tip (in integer).
+#b) Calculate, set the result to two decimal places and print the result.
+
+bill= float(input("Enter the bill amount :$"))
+tip=int(input("Enter the tip percentage :"))
+tipAmount= (tip/100)*bill
+
+bill= float(input("Enter the bill amount :$"))
+tip=int(input("Enter the tip percentage :"))
+tipAmount= (tip/100)*bill
---
+++ pyton3/PE3_3.py
@@ -0,0 +1,7 @@
+#Calculate the BMI. Save the code as PE3_3.py.
+#a) Prompt and request the input for height(cm) and weight(kg) from the console.
+#b) Compute the body mass index and print it in three decimal places.
+#Input text can be any content. Just make sure to precisely match the output format below.
+height= float(input("Enter the height :$"))
+weight=int(input("Enter the weight :"))
+tipAmount= (tip/100)*bill
